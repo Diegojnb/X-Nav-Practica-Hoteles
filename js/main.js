cda20ed3bdc80296f60464af5454a39f1b01bd65
diegojnb <diegojnb@gmail.com>
2016-05-19 23:39:01 +0200

    Resuelto eliminado de popups y su aparicion de info

--- js/main.js
+++ js/main.js
@@ -1,3 +1,111 @@
+var alojamientos;
+var Lpopup=[];
+var marcadores=[];
+var mostrando;
+
+function deletethis(lat,lon){
+	var layer;
+	console.log( Lpopup.length);
+	for (var i = 0; i < Lpopup.length; i++) {
+
+		if (Lpopup[i]._latlng.lat == lat & Lpopup[i]._latlng.lon == lon) {
+			layer = Lpopup[i];
+			Lpopup.splice(i,1);
+		}
+	}
+
+	for (var i = 0; i < marcadores.length; i++) {
+		if (marcadores[i].lat == lat & marcadores[i].lon == lon) {
+			marcadores.splice(i,1);
+		}
+	}
+
+ 	mymap.removeLayer(layer);
+
+}
+
+function addmarker(lat,lon){
+	var aparece = false;
+	var marcador={};
+	marcador.lat = lat;
+	marcador.lon = lon;
+	if (marcadores.length == 0) {
+		marcadores.push(marcador);
+	}else{
+		for (var i = 0; i < marcadores.length; i++) {
+			if (marcadores[i].lon == lon & marcadores[i].lat == lat) {
+				aparece = true;
+				break;
+			}
+		}
+		if (!aparece) {
+			marcadores.push(marcador);
+		}
+	}
+}
+
+function show_accomodation(numero){
+	if ($(this).attr('numero') !== undefined) {
+		var numero = $(this).attr('numero');
+	}
+	mostrando = numero
+	var accomodation = alojamientos[numero];
+	var lat = accomodation.geoData.latitude;
+	var lon = accomodation.geoData.longitude;
+	var url = accomodation.basicData.web;
+	var name = accomodation.basicData.name;
+	var desc = accomodation.basicData.body;
+
+	var imgs = new Array();
+
+	accomodation.multimedia.media.forEach(function(el,index){
+		imgs[index] = accomodation.multimedia.media[index].url;
+	});
+	var carousel = "";
+	if (imgs.length > 0) {
+		carousel += '<div id="myCarousel" class="carousel slide" data-interval="3000" data-ride="carousel">' +
+								'<ol class="carousel-indicators">';
+		imgs.forEach(function(el,index){
+			if (index == 0) {
+				carousel += '<li data-target="#myCarousel" data-slide-to="' + index + '" class="active"></li>';
+			}else{
+					carousel += '<li data-target="#myCarousel" data-slide-to="' + index + '"</li>';
+			}
+		});
+		carousel += '</ol><div class="carousel-inner">';
+		imgs.forEach(function(el,index){
+			if (index == 0) {
+				carousel += '<div class="item active"><img src="' + el +'"></div>';
+			}else{
+				carousel += '<div class="item"><img src="' + el +'"></div>';
+			}
+		});
+		carousel += '<a class="carousel-control left" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a><a class="carousel-control right" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>';
+	}
+
+	var cat = accomodation.extradata.categorias.categoria.item[1]['#text'];
+	var subcat = accomodation.extradata.categorias.categoria.subcategorias.subcategoria.item[1]['#text'];
+
+	addmarker(lat,lon);
+
+	var popup = L.marker([lat, lon]);
+	popup._latlng.lon = lon;
+	Lpopup.push(popup);
+
+	popup.addTo(mymap)
+	 .bindPopup('<a title="' + numero + '" href="' + url + '">' + name + '</a><button class="glyphicon glyphicon-trash" onClick="deletethis(\''+lat+'\',\''+lon+'\');" ></button>')
+	 .openPopup();
+
+	mymap.setView([lat, lon], 15);
+
+	inner = "";
+	inner += "<h2>" + name + "</h2>" +  "<h3>" + cat + " - " +
+						subcat + "</h3>" + desc + carousel;
+
+	$("#info").html(inner);
+
+};
+
 $(document).ready(function(){
 
 	mymap = L.map('mapid').setView([40.419578500000, -3.698562600000], 13);
@@ -8,64 +116,17 @@ $(document).ready(function(){
 
 	$('.window').click(function (e) {
 	  $(this).tab('show')
-	})
+	});
 
-  function show_accomodation(){
-    var accomodation = alojamientos[$(this).attr('numero')];
-		var numero = $(this).attr('numero');
-    var lat = accomodation.geoData.latitude;
-    var lon = accomodation.geoData.longitude;
-    var url = accomodation.basicData.web;
-    var name = accomodation.basicData.name;
-    var desc = accomodation.basicData.body;
-
-		var imgs = new Array();
-
-		accomodation.multimedia.media.forEach(function(el,index){
-			imgs[index] = accomodation.multimedia.media[index].url;
-		});
-		var carousel = "";
-		if (imgs.length > 0) {
-			carousel += '<div id="myCarousel" class="carousel slide" data-interval="3000" data-ride="carousel">' +
-											'<ol class="carousel-indicators">';
-			imgs.forEach(function(el,index){
-				if (index == 0) {
-					carousel += '<li data-target="#myCarousel" data-slide-to="' + index + '" class="active"></li>';
-				}else{
-					carousel += '<li data-target="#myCarousel" data-slide-to="' + index + '"</li>';
-				}
-			});
-			carousel += '</ol><div class="carousel-inner">';
-			imgs.forEach(function(el,index){
-				if (index == 0) {
-					carousel += '<div class="item active"><img src="' + el +'"></div>';
-				}else{
-					carousel += '<div class="item"><img src="' + el +'"></div>';
-				}
-			});
-			carousel += '<a class="carousel-control left" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a><a class="carousel-control right" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>';
+	mymap.on('popupopen', function() {
+		if (mostrando != $(this._popup._content).attr('title')) {
+			show_accomodation($(this._popup._content).attr('title'));
 		}
-
-    var cat = accomodation.extradata.categorias.categoria.item[1]['#text'];
-    var subcat = accomodation.extradata.categorias.categoria
-     .subcategorias.subcategoria.item[1]['#text'];
-
-    L.marker([lat, lon]).addTo(mymap)
-  	 .bindPopup('<a title="' + numero + '" href="' + url + '">' + name + '</a>')
-  	 .openPopup();
-
-    mymap.setView([lat, lon], 15);
-
-		inner = "";
-		inner += "<h2>" + name + "</h2>" +  "<h3>" + cat + " - " +
-			subcat + "</h3>" + desc + carousel;
-
-		$("#info").html(inner);
-  };
+	});
 
   $("#cargarJ").click(function() {
-    $.getJSON("json/alojamientos.json")
-    .done(function(data){
+    $.getJSON("json/alojamientos.json").done(function(data){
+
 			alojamientos = data.serviceList.service;
 
 			$("#listahoteles").empty();
@@ -89,13 +150,14 @@ $(document).ready(function(){
     });
   });
 
-
-
 	$("#crearC").click(function() {
 		var colection = prompt("Nombre de la coleccion", "Hoteles 4 estrellas");
 		if (colection != null) {
+
 				inner = $("#listacolecciones").html()
-				inner += '<div class="dropdown droppable ui-state-default ui-droppable"><button class="btn btn-secondary dropdown-toggle " data-toggle="dropdown">' +colection + '</button><div class="dropdown-menu" ></div></div>';
+				inner += '<div class="dropdown droppable ui-state-default ui-droppable">'+
+								 '<button class="btn btn-secondary dropdown-toggle " data-toggle="dropdown">' +
+				 			    colection + '</button><div class="dropdown-menu" ></div></div>';
 				$("#listacolecciones").html(inner);
 
 				$(".droppable" ).droppable({
@@ -109,9 +171,16 @@ $(document).ready(function(){
 						$(this).html(res);
 					}
 				});
-				
 		}else{
 			alert("Nombre no valido")
 		}
+	});
+
+	$("#borrarC").click(function(){
+		$("#listacolecciones *").click(function(){
+			if ($(this).html().contains("button")) {
+				$(this).empty();
+			}
+		});
 	});
 });
